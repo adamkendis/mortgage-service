04889cdaf42ce41015f955b490d86ac26ddda67e
Adam Kendis <atkendis@gmail.com>
2018-11-25 13:16:34 -0800

    Reverted data generation script to output 1 csv file. Seeding cassandra via cqlsh.

--- server/database/index.js
+++ server/database/index.js
@@ -1,10 +1,37 @@
-const mongoose = require('mongoose');
+// const cassandra = require('cassandra-driver');
+const models = require('express-cassandra');
+// const async = require('async');
+// const db = new cassandra.Client({contactPoints: ['127.0.0.1'], keyspace: 'mortgage'});
 
-const uri = 'mongodb://localhost/houses';
+models.setDirectory(__dirname + '/models').bind(
+  {
+    clientOptions: {
+      contactPoints: ['127.0.0.1'],
+      protocolOptions: { port: 9042 },
+      keyspace: 'mortgage',
+      queryOptions: { consistency: models.consistencies.one }
 
-const db = mongoose.connect(
-  uri,
-  { useNewUrlParser: true },
+    },
+    ormOptions: {
+      defaultReplicationStrategy: {
+        class: 'SimpleStrategy',
+        replication_factor: 1
+      },
+      migration: 'safe',
+      createKeyspace: true
+    }
+  },
+  function(err) {
+    if (err) {
+      throw err;
+    }
+  }
 );
 
-module.exports = db;
+models.import(__dirname + '/data1.csv', function(err) {
+  if (err) {
+    throw err;
+  }
+});
+
+// export default db;
