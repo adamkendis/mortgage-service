05684613b1d72060e54b60c0221f117223a5d939
Adam Kendis <atkendis@gmail.com>
2018-11-29 16:55:19 -0800

    Installed redis and added caching to node server.

--- server/server.js
+++ server/server.js
@@ -4,15 +4,51 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const path = require('path');
 const app = express();
+const redis = require('redis');
+const client = redis.createClient();
 
 import schema from './schema.js';
+
+const knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'mortgage'
+  },
+  pool: { min: 0, max: 7 }
+});
 
 app.use(cors());
 app.use(express.static(`${__dirname}/../public`));
 
+const getHouse = (req, res) => {
+  let houseId = req.params.urlId;
+  knex('house').where('_id', houseId)
+    .then(data => {
+      client.set(houseId, data, 'EX', 3600);
+      res.send(data); 
+    })
+    .catch(err => console.log('Error sending data to client: ', err))
+}
+
+const getCache = (req, res) => {
+  let houseId = req.params.urlId;
+  client.get(houseId, (err, result) => {
+    if (result) {
+      res.send(result);
+    } else {
+      getHouse(req, res);
+    }
+  });
+}
+
 app.get('/:urlId', (req, res) => {
   res.sendFile(path.join(`${__dirname}/../public/index.html`));
 });
+
+app.get('/homes/:urlId', getCache);
 
 app.use(
   '/graphql',
